46e33737e04b134cc995f64498829d7afacc1b4a
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2025-03-03 22:20:29 +0530

    Game website

--- GameZone/src/App.jsx
+++ GameZone/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react'
+import Hero from './components/Hero.jsx'
 
-function App() {
+const App = () => {
   return (
     <>
-      
+      <main className='relative min-h-screen w-screen overflow-x-hidden'>
+        <Hero/>
+      </main>
     </>
   )
 }
