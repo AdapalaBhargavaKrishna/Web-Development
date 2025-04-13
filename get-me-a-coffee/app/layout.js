79190be53f828e9503394c4ae54149b3859ef7b7
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2025-04-13 22:41:17 +0530

    Started a Patreom clone

---
+++ get-me-a-coffee/app/layout.js
@@ -0,0 +1,37 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import SessionWrapper from "@/components/SessionWrapper";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Get me a Coffee - Fuel your passion, one cup at a time",
+  description: "A Platform to support your favorite creators",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="scroll-smooth">
+      <body className="bg-black bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] text-white">
+
+        <SessionWrapper>
+          <Navbar />
+        <div className="min-h-screen bg-black bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] text-white">
+        {children}
+        </div>
+        <Footer />
+        </SessionWrapper>
+      </body>
+    </html>
+  );
+}
---
+++ get-me-a-coffee/app/page.js
@@ -0,0 +1,47 @@
+export default function Home() {
+  return (
+    <>
+    <div className="flex justify-center flex-col items-center gap-4 text-white h-[44vh] ">
+      <div className="font-bold flex gap-2 text-5xl justify-center items-center">Get Me a Coffee <span><img src="/tea.gif" width={80} alt="" /></span></div>
+      <p>
+      A Platform to support your favorite creators
+      </p>
+      <div>
+      <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Now</button>
+      <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Read More</button>
+      </div>
+    </div>
+    <div className="bg-white h-1 opacity-10">
+    </div>
+
+      <div className="text-white container mx-auto pb-32 pt-14">
+        <h1 className="text-3xl font-bold text-center mb-14">Your Fans can buy you a coffee</h1>
+        <div className="flex gap-5 justify-around">
+          <div className="item space-y-3 flex flex-col items-center justify-center">
+            <img className="bg-slate-400 rounded-full p-2" width={88} src="/man.gif" alt="" />
+            <p className="font-bold">Fans want to help</p>
+            <p className="text-center">Your fans are available for you to help you</p>
+          </div>
+          <div className="item space-y-3 flex flex-col items-center justify-center">
+            <img className="bg-slate-400 rounded-full p-2" width={88} src="/coin.gif" alt="" />
+            <p className="font-bold">Fans want to help</p>
+            <p className="text-center">Your fans are available for you to help you</p>
+          </div>
+          <div className="item space-y-3 flex flex-col items-center justify-center">
+            <img className="bg-slate-400 rounded-full p-2" width={88} src="/group.gif" alt="" />
+            <p className="font-bold">Fans want to help</p>
+            <p className="text-center">Your fans are available for you to help you</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-white h-1 opacity-10">
+      </div>
+
+      <div className="text-white container mx-auto pb-32 pt-14 flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold text-center mb-14">Learn more about us</h1>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/fjHO4fAfCf0?si=BhabCdGULsbr6lNC" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      </div>
+    </>
+  );
+}
