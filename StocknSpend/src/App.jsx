4bad719695dd494ff0c6994e665eb69b1dce4b3f
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2025-04-16 21:21:54 +0530

    No idea to proceed further

--- StocknSpend/src/App.jsx
+++ StocknSpend/src/App.jsx
@@ -1,13 +1,25 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Layout from './components/Layout'
+import Home from './components/Home'
+import Stocks from './components/Stocks'
+import Finance from './components/Finance'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
-
   return (
-    <>
-      <Navbar />
-    </>
+    <Router>
+      <Layout>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/stocks" element={<Stocks />} />
+          <Route path="/finance" element={<Finance />} />
+          {/* Add more routes as you create more components! */}
+        </Routes>
+      </Layout>
+    </Router>
   )
 }
 
-export default App
+export default App;
---
+++ StocknSpend/src/components/Finance.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Finance = () => {
+  return (
+    <>
+       
+    </>
+  )
+}
+
+export default Finance
