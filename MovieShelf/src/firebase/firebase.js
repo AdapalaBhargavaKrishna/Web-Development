a90f27173a19f65ea68de81c8a69f3dcc0fca9c2
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2025-04-19 12:04:11 +0530

    Movie Shelf

--- MovieShelf/src/firebase/firebase.js
+++ MovieShelf/src/firebase/firebase.js
@@ -3,21 +3,17 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from
 import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDDl0dZSBkIQ9drb2XoKLhv_khrsAkBS24",
-    authDomain: "musicc-shelf.firebaseapp.com",
-    projectId: "musicc-shelf",
-    storageBucket: "musicc-shelf.firebasestorage.app",
-    messagingSenderId: "329466960097",
-    appId: "1:329466960097:web:bf4981cf16fd07dc06ffa0",
-    measurementId: "G-0096D6NX4N"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Authentication and Firestore
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Export everything you use
 export { auth, db, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, doc, setDoc, getDoc };
