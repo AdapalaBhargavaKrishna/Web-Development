b9fa38b02d8575242b3c8081b9ac0a5f1c12b350
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2025-06-20 11:26:30 +0530

    Added Toasts

--- AutoTube/Frontend/src/App.jsx
+++ AutoTube/Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Home from './pages/Home'
@@ -10,6 +11,7 @@ import NotFound from './pages/NotFound'
 const App = () => {
   return (
     <Router>
+      <Toaster position="top-center" reverseOrder={false} />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/Signup" element={<Signup />} />
