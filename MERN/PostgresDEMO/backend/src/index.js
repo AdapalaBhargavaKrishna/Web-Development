9d6890d6fa2abeba943b064c4de99f94593c4a06
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2026-03-01 08:44:05 +0530

    Prisma

---
+++ MERN/PostgresDEMO/backend/src/index.js
@@ -0,0 +1,12 @@
+const prisma = require('./db.js')
+
+async function main() {
+
+    // -----------------------CREATE----------------------- 
+
+
+}
+
+main()
+    .catch(console.error)
+    .finally(() => prisma.$disconnect()) 
